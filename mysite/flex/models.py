188102eb85e954647726731d013f71b7d16a46fe
Radim Közl <kozl@svmetal.cz>
2025-03-18 00:59:18 +0100

    feat: Add flex page with StreamField

---
+++ mysite/flex/models.py
@@ -0,0 +1,31 @@
+"""Flexible page."""
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+
+from streams import blocks
+
+class Flex(Page):
+    """Flexible page class."""
+    template = "flex/flex_page.html"
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock())
+        ],
+        null=True,
+        blank=True
+    )
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('content'),
+    ]
+    
+    class Meta:
+        verbose_name = "Flexible Page"
+        verbose_name_plural = "Flexible Pages"
