452c7fb62da2ea7e7d48a44a1cca018009777b08
Radim Közl <kozl@svmetal.cz>
2025-03-23 23:43:14 +0100

    feat: Add Call to action StreamFields

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -3,7 +3,11 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, PageChooserPanel
-#from wagtail.images.edit_handlers import ImageChooserPanel
+
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+
+from streams import blocks
 
 
 class HomePage(Page):
@@ -36,11 +40,20 @@ class HomePage(Page):
         related_name='+'
     )
     
+    content = StreamField(
+        [
+            ("cta", blocks.CTABlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    
     content_panels = Page.content_panels + [
         FieldPanel('banner_title'),
         FieldPanel('banner_subtitle'),
         FieldPanel('banner_image'),
-        PageChooserPanel('banner_cta')
+        PageChooserPanel('banner_cta'),
+        FieldPanel('content'),
     ]
     
     class Meta:
--- mysite/streams/blocks.py
+++ mysite/streams/blocks.py
@@ -58,3 +58,19 @@ class SimpleRichTextBlock(blocks.RichTextBlock):
         template = "streams/rich_text_block.html"
         icon = "edit"
         label = "Simple RichText"
+        
+
+class CTABlock(blocks.StructBlock):
+    """Simple call to action block."""
+    
+    title = blocks.CharBlock(required=True, max_length=60)
+    text = blocks.RichTextBlock(required=True, features=["bold", "italic"])
+    button_page = blocks.PageChooserBlock(required=False)
+    button_url = blocks.URLBlock(required=False) # If the button page is selected, this is not used.
+    button_text = blocks.CharBlock(required=True, default="Learn More", max_length=40)
+    
+    class Meta:  #noqa
+        template = "streams/cta_block.html"
+        icon = "placeholder"
+        label = "Call to Action"
+    
