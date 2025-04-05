e5dc053d431fcf7527104adcf0c08f382740d97d
Radim Közl <kozl@svmetal.cz>
2025-04-05 16:02:37 +0200

    feat: Add Select multiple blog authors (snippets + orderables)

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,13 +3,31 @@
 from django.db import models
 from django.shortcuts import render
 
-from wagtail.models import Page
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
+from wagtail.admin.panels import (
+    FieldPanel, 
+    MultiFieldPanel, 
+    InlinePanel
+)
 from wagtail.fields import StreamField
 from wagtail.contrib.routable_page.models import RoutablePageMixin, re_path
 from wagtail.snippets.models import register_snippet
 
 from streams import blocks
+
+class BlogAuthorsOrderable(Orderable):
+    """This allow us to select one or more authors from Snippets."""
+    page = ParentalKey("blog.BlogDetailPage", related_name="blog_authors")
+    author = models.ForeignKey(
+        "blog.BlogAuthor",
+        on_delete=models.CASCADE,
+    )
+    
+    panels = [
+        FieldPanel('author'),
+    ]
 
 class BlogAuthor(models.Model):
     """Blog author for snippets."""
@@ -132,5 +150,11 @@ class BlogDetailPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('custom_title'),
         FieldPanel('blog_image'),
+        MultiFieldPanel(
+            [
+                InlinePanel('blog_authors', label="Author", min_num=1, max_num=4),
+            ],
+            heading="Author(s)",
+        ),
         FieldPanel('content'),
     ]
